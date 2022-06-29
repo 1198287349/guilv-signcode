6c77129842f40a8433788c40970fd5c7e756ad14
Android玄机 <38068634+1198287349@users.noreply.github.com>
2022-06-29 17:45:27 +0800

    Update signcode.java

--- src/com/demo/signcode.java
+++ src/com/demo/signcode.java
@@ -9,10 +9,19 @@ import java.util.TreeMap;
 
 class signcode {
 
-    //这里输入账号密码
-    public static void main(String[] args) {
-        getSignCode("18541214008" , "jpy890626");
-    }
+    //省略关键核心代码
+    //省略关键核心代码
+    //省略关键核心代码
+    //省略关键核心代码
+    //省略关键核心代码
+    //省略关键核心代码
+    //省略关键核心代码
+    //省略关键核心代码
+    //省略关键核心代码
+    //省略关键核心代码
+    //省略关键核心代码
+    //省略关键核心代码
+    //省略关键核心代码
 
     private static String getSignCode(String account, String pwd) {
         String sortString="";
@@ -21,12 +30,11 @@ class signcode {
             {
 
 
-                put("version", "3.0.0");
-                put("client", "android");
-                put("deviceId", "7528c0ca-9c03-3696-9a75-01d7bd7797fb");
-                put("timestamp", "1655721479978");
-                put("encrypt", "1");
-                put("appkey", "gl_rn_bdc5bd4f38b6");
+                put("version", "3.1.0");
+                put("", "");
+                put("", "");
+                put("", "");
+                put("", "");
                 put("uinfo", account);
                 put("password", pwd);
             }
@@ -41,53 +49,33 @@ class signcode {
     }
 
 
-    // 加盐
+    
     public static String getSaltCode(String str) {
-        String salt="57F6A245D65F3B86CC58103E84C04C8F";
+        String salt="联系QQ1198287349";
         String saltString=salt+str+salt;
         System.out.println("getSaltCode==================" + saltString);
         String upString = getUpString(saltString);
         return upString;
     }
 
-    // 转化为大小写
-    private static String getUpString(String str)  {
-        String strUpString=str.toUpperCase();
-        System.out.println("getUpString==================" + strUpString);
-
+  
         String md5CodeString=  getMD5Str(strUpString);
         System.out.println("signcode==================" + md5CodeString);
         return md5CodeString;
     }
 
+    
+//省略关键核心代码
+//省略关键核心代码   
+//省略关键核心代码    
+//省略关键核心代码
+//省略关键核心代码    
+//省略关键核心代码        
+//省略关键核心代码
+//省略关键核心代码
 
 
-    private static Map<String, String> sortMap(Map<String, String> param) {
-        TreeMap<String, String> paramTreeMap = new TreeMap<>(param);
-        System.out.println(paramTreeMap);
-        return paramTreeMap;
-    }
-
-
-    public static String getMD5Str(String val)  {
-        try{
-            MessageDigest md5 = MessageDigest.getInstance("MD5");
-            md5.update(val.getBytes());
-            byte[] m = md5.digest();// 加密
-            return getString(m);
-        }catch (Exception e){
-
-        }
-        return "";
-    }
-
-    /**
-     *
-     * @param bytearray 加密后的byte数组
-     * @return byte数组转换成一个16进制字符串
-     *
-     * toHexString方法是把int类型转换为16进制的字符串
-     */
+   
     private static String getString(byte[] bytearray) {
         StringBuffer sb = new StringBuffer();
         for (byte b : bytearray) {
